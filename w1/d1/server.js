d2a49daa1cc0dad56b63f558bcaa421ef0cdb9cf
Yuto Yamakita <yuvto.0420@gmail.com>
2022-09-07 15:58:01 -0700

    Answer Q1

--- w1/d1/server.js
+++ w1/d1/server.js
@@ -2,9 +2,20 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("request received", req.url);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World!!!!!!!</h1>");
-  res.end();
+  
+  if(req.url == '/dog'){
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Dog. woof</h1>");
+    res.end();
+  } else if(req.url == '/cat'){
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Cat. meow</h1>");
+    res.end();
+  }else{
+    res.setHeader("Content-Type", "text/html");
+    res.write("<h1>Hello World!!!!!!!</h1>");
+    res.end();
+  }
 });
 
 server.listen(3000, () => {
